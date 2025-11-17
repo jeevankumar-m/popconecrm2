d2d2e45b923cfaec4351f68829a14c574c2a8ebd
jeevankumar-m <jeevankumar06m@gmail.com>
2025-11-17 20:30:19 +0530

    redeploying to fix vercel issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Contacts from './pages/Contacts'
 import ContactDetails from './pages/ContactDetails'
 import Login from './pages/Login'
